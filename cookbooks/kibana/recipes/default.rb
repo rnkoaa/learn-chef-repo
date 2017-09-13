bd4a785b14cf8003f8e1cc045ed65cc0c0b8c3c4
Richard Agyei <rnkoaa@users.noreply.github.com>
2017-09-13 13:31:15 -0500

    update kibana with config file

--- cookbooks/kibana/recipes/default.rb
+++ cookbooks/kibana/recipes/default.rb
@@ -20,5 +20,18 @@ package 'kibana' do
 end
 
 service 'kibana' do
+    action :stop
+    ignore_failure true
+end
+
+cookbook_file "/etc/kibana/kibana.yml" do
+    source "kibana.yml"
+    owner "kibana"
+    group "kibana"
+    mode 0600
+    notifies :restart, "service[kibana]"
+end
+
+service 'kibana' do
     action [:enable, :start]
 end
