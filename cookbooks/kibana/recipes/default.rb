b5865ab6f64632170b6cff53227c7d40806a22d8
Richard Agyei <rnkoaa@users.noreply.github.com>
2017-09-13 13:26:13 -0500

    kibana

--- cookbooks/kibana/recipes/default.rb
+++ cookbooks/kibana/recipes/default.rb
@@ -1,5 +1,24 @@
+
 #
 # Cookbook:: kibana
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+yum_repository 'kibana' do
+    description "Kibana 5.x repo"
+    baseurl "https://artifacts.elastic.co/packages/5.x/yum"
+    gpgkey "https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+    enabled true
+    gpgcheck true
+    fastestmirror_enabled true
+    action :create
+end
+
+package 'kibana' do
+    action :install
+end
+
+service 'kibana' do
+    action [:enable, :start]
+end
