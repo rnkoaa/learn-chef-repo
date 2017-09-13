e216b18382b3fac2e8182303767326d2d1b4f643
Richard Agyei <rnkoaa@users.noreply.github.com>
2017-09-13 13:44:27 -0500

    kibana (#1)
    * kibana
    
    * create kibana.yml file for config
    
    * update kibana with config file
    
    * Create .gitkeep
    
    * conf files for processing logs
    
    * logstash conf file
    
    * logstash conf directories

--- cookbooks/kibana/recipes/default.rb
+++ cookbooks/kibana/recipes/default.rb
@@ -1,5 +1,37 @@
+
 #
 # Cookbook:: kibana
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+yum_repository 'kibana' do
+    description "Kibana 5.x repo"
+    baseurl "https://artifacts.elastic.co/packages/5.x/yum"
+    gpgkey "https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+    enabled true
+    gpgcheck true
+    fastestmirror_enabled true
+    action :create
+end
+
+package 'kibana' do
+    action :install
+end
+
+service 'kibana' do
+    action :stop
+    ignore_failure true
+end
+
+cookbook_file "/etc/kibana/kibana.yml" do
+    source "kibana.yml"
+    owner "kibana"
+    group "kibana"
+    mode 0600
+    notifies :restart, "service[kibana]"
+end
+
+service 'kibana' do
+    action [:enable, :start]
+end
--- cookbooks/logstash/recipes/default.rb
+++ cookbooks/logstash/recipes/default.rb
@@ -3,3 +3,40 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+yum_repository 'Logstash' do
+    description "Logstash 5.x repo"
+    baseurl "https://artifacts.elastic.co/packages/5.x/yum"
+    gpgkey "https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+    enabled true
+    gpgcheck true
+    fastestmirror_enabled true
+    action :create
+end
+
+package 'logstash' do
+    action :install
+end
+
+service 'logstash' do
+    action :stop
+    ignore_failure true
+end
+
+cookbook_file "/etc/logstash/logstash.yml" do
+    source "logstash.yml"
+    owner "logstash"
+    group "logstash"
+    mode 0600
+    notifies :restart, "service[logstash]"
+end
+
+remote_directory '/etc/logstash/conf.d' do
+  source 'conf.d'
+  owner "logstash"
+  group "logstash"
+  action :create
+end
+
+service 'logstash' do
+    action [:enable, :start]
+end
